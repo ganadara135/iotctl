0e3d79f4076dc78b636c715532c3eb9697121aec
ywnwalone <ywnwalone@naver.com>
2017-08-22 14:06:13 +0900

    fix

--- router/main.js
+++ router/main.js
@@ -271,14 +271,7 @@ app.post('/requestAllDeviceList',urlencodedParser, function(req, res){
           for(x in devicesOf){
             result[x] = devicesOf[x];
             result[x].myDevice = 0;     // 내 장치가 아님
-<<<<<<< HEAD
-            for(y in relationshipOf){
-              if(relationshipOf[y].userAddress == userAddress){
-//                console.log(" relationshipOf[x].userAddress.   --> : ", relationshipOf[y].userAddress)
-                result[x].myDevice = 1;   // 내 장치 임
-                console.log("devicesOf["+x+"]", devicesOf[x]);
-                console.log("result["+x+"]", result[x]);
-=======
+
           }
 
           for(y in relationshipOf){
@@ -289,7 +282,6 @@ app.post('/requestAllDeviceList',urlencodedParser, function(req, res){
                 if(devicesOf[x].address == relationshipOf[y].deviceAddress){
                   result[x].myDevice = 1;   // 내 장치 임
                 }
->>>>>>> 36e513d3c46bd6b4ef4f2ca9fa1fbbf6f166b04e
               }
             }
           }
